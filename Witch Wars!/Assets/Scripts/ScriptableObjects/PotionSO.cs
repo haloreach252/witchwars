ca0f257f5c1cbc65f1e1a59d41b3898025e36b3e
haloreach252 <haloreach252@gmail.com>
2018-06-27 12:25:12 -0600

    Added a tower, a basic damage potion, and changed the script for potions a little

---
+++ Witch Wars!/Assets/Scripts/ScriptableObjects/PotionSO.cs
@@ -0,0 +1,27 @@
+﻿/*
+* Copyright (c) Engineer Industries
+* https://www.youtube.com/channel/UC-v_UB0BIwIoKlcq2ZoRmFg
+*/
+
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "New Potion", menuName = "Custom/Items/Potion", order = 1)]
+public class PotionSO : ScriptableObject {
+
+	public string potionName;
+	[TextArea]
+	public string potionDescription;
+	public float potionRadius;
+	public float potionDamage;
+	public enum PotionEffect
+	{
+		DAMAGE,
+		POISON,
+		FREEZE,
+		FIRE,
+		HEALING
+	}
+	public PotionEffect potionEffect;
+
+}
